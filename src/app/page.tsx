ed757844d196e2bca4e74d6e22c374c0ab6fb635
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-08 18:09:50 +1300

    edit function + prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,17 @@ async function toggleTodo(id: string, complete: boolean) {
   await prisma.todo.update({ where: { id }, data: { complete } })
 }
 
+async function editTodo(id: string, title: string) {
+  'use server'
+  await prisma.todo.update({ where: { id }, data: { title } })
+}
 export default async function Home() {
   const todos = await getTodos()
 
   return (
     <>
       <header className="flex justify-between mb-4 items-center">
-        <h1 className="text-4xl align-self=center ml-12">Todos</h1>
+        <h1 className="text-4xl align-self-center ml-12">Todos</h1>
         <Link
           href="/new"
           className="border boder-slate-300 px-2 py-1 rounded hover:bg-slate-700 focys-within:bg-slate-700 outline-none"
@@ -27,7 +31,12 @@ export default async function Home() {
 
       <ul className="pl-4 flex flex-col items-center text-3xl mt-12">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+          <TodoItem
+            key={todo.id}
+            {...todo}
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+          />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,26 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
 type TodoItemProps = {
   id: string
   title: string
   complete: boolean
   toggleTodo: (id: string, complete: boolean) => void
+  editTodo: (id: string, title: string) => void
 }
 
-export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+export function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+  editTodo,
+}: TodoItemProps) {
+  const [editing, setEditing] = useState(false)
+  const [input, setInput] = useState('')
+  const router = useRouter()
   return (
     <li className="flex gap-1 items-center">
       <input
@@ -17,12 +30,35 @@ export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
         defaultChecked={complete}
         onChange={(e) => toggleTodo(id, e.target.checked)}
       />
-      <label
-        htmlFor={id}
-        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
-      >
-        {title}
-      </label>
+      {!editing && (
+        <label
+          htmlFor={id}
+          className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+          onDoubleClick={() => setEditing(true)}
+        >
+          {title}
+        </label>
+      )}
+      {editing && (
+        <>
+          <input
+            className="text-black"
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+            defaultValue={title}
+          />
+          <button
+            type="button"
+            onClick={() => {
+              setEditing(false)
+              editTodo(id, input)
+              router.refresh()
+            }}
+          >
+            Save
+          </button>
+        </>
+      )}
     </li>
   )
 }
